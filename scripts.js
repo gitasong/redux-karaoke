46419db52474dd2a3b623ca3063e800c969efb41
Nicole Freed <nicole.freed@gmail.com>
2017-09-18 10:36:32 -0700

    create index.html, scripts.js w/store & initialState

--- scripts.js
+++ scripts.js
@@ -1,3 +1,26 @@
+const chorus = "Don't want to be a fool for you, Just another player in your game for two, You may hate me but it ain't no lie, Baby bye bye bye, Bye bye, I Don't want to make it tough, I just want to tell you that I've had enough, It might sound crazy but it ain't no lie, Baby bye bye bye";
+
+const chorusArray = chorus.split(', ');
+
+const initialState = {
+  chorus: chorus,
+  chorusArray: chorusArray,
+  position: 0,
+  currentPhrase: chorusArray[0]
+}
+
+console.log(initialState);
+
+// REDUCER
+
+const { expect } = window;
+
+// UNIT TESTS
+
+const { createStore } = Redux;
+const store = createStore(reducer);
+console.log(store.getState());
+
 const userClick = () => {
   console.log('click');
 }
